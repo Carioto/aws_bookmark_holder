25ae4acbf6d5e9eb6d1f3b6f734bd71664838209
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-07-28 16:59:02 -0400

    heroku setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,54 +6,54 @@ import react from "@vitejs/plugin-react";
 
 
 
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-  // if (command === 'serve') {
-  //   return {
-  //     plugins: [react()],
-  //       server: {
-  //         port: 3000,
-  //         open: true,
-  //         proxy: {
-  //           "/graphql": {
-  //             target: "http://localhost:3001",
-  //             changeOrigin: true,
-  //             secure: false,
-  //           },
-  //         },
-  //       },
-  //   }
-  // } else {
-    // command === 'build'
-    return {
-      plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      "/graphql": {
-        target: "https://bookmark-server-e0d8f2210d08.herokuapp.com/",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-//   },    }
-  }
-}
-})
-
-// export default defineConfig({
-//   plugins: [react()],
+// export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+//   if (command === 'serve') {
+//     return {
+//       plugins: [react()],
+//         server: {
+//           port: 3000,
+//           open: true,
+//           proxy: {
+//             "/graphql": {
+//               target: "http://localhost:3001",
+//               changeOrigin: true,
+//               secure: false,
+//             },
+//           },
+//         },
+//     }
+//   } else {
+//     // command === 'build'
+//     return {
+//       plugins: [react()],
 //   server: {
 //     port: 3000,
 //     open: true,
 //     proxy: {
 //       "/graphql": {
-//         target: "https://bookmark-holder-36a65b094507.herokuapp.com/",
+//         target: "https://bookmark-server-e0d8f2210d08.herokuapp.com/",
 //         changeOrigin: true,
 //         secure: false,
 //       },
 //     },
-//   },
-// });
+// //   },
+//     }
+//   }
+// }})
 
-// http://localhost:3001
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
+
+// 
