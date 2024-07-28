cd52cc7ae4b6244b2793c098e47faec441ccd2ef
David Carioto <148878612+Carioto@users.noreply.github.com>
2024-07-27 20:35:37 -0400

    heroku

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,17 +2,58 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+
+
+
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+        server: {
+          port: 3000,
+          open: true,
+          proxy: {
+            "/graphql": {
+              target: "http://localhost:3001",
+              changeOrigin: true,
+              secure: false,
+            },
+          },
+        },
+    }
+  } else {
+    // command === 'build'
+    return {
+      plugins: [react()],
   server: {
     port: 3000,
     open: true,
     proxy: {
       "/graphql": {
-        target: "http://localhost:3001",
+        target: "https://bookmark-holder-36a65b094507.herokuapp.com/",
         changeOrigin: true,
         secure: false,
       },
     },
-  },
-});
+//   },
+    }
+  }
+}})
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       "/graphql": {
+//         target: "https://bookmark-holder-36a65b094507.herokuapp.com/",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
+// http://localhost:3001
